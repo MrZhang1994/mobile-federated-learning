5bd2966995dd4917110d68760c538e56145d5e86
zyayoung <1328410180@qq.com>
2021-01-05 23:19:11 +0800

    direct run

--- run_all_rl.sh
+++ run_all_rl.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+cd src
 for method in ddpg pg random ddpg_baseline pg_amender pg_noamender
 do
-    RL_PRESET=$method ./begin.sh
+    RL_PRESET=$method python3 ./main_fedavg.py \
+    --gpu 0 \
+    --dataset femnist \
+    --data_dir ../FedML/data/FederatedEMNIST/datasets \
+    --model cnn \
+    --partition_method hetero  \
+    --comm_round 59361 \
+    --batch_size 100 \
+    --client_optimizer sgd \
+    --lr 0.03 \
+    --method sch_mpn \
+    -f
 done
