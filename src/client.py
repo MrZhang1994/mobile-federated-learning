f76be38807330fedef6c7919988a972609892de4
zzp1012 <zzp1012@sjtu.edu.cn>
2021-01-18 00:49:51 +0800

    update: bug-fix

--- src/client.py
+++ src/client.py
@@ -71,7 +71,7 @@ class Client:
             # if grads meets something strange, we will terminate the training process.
             if torch.isnan(grads).sum() > 0 or torch.isnan(loss) or torch.norm(grads) > self.args.lr * THRESHOLD_GRADS_RATIO * torch.norm(last_w):
                 logger.warning("grads {} too large than weights {} or meets nan with epoch {}".format(torch.norm(grads), torch.norm(last_w), epoch))
-                return net.cpu().state_dict(), None, None, None, None
+                return net.cpu().state_dict(), None, None, None
             optimizer.step() # updates weights
 
             loss = loss.item() # get current loss
